6ed9dbfb896bad968f36936b252af9002796eff6
eswaribala <parameswaribala@gmail.com>
2022-03-24 11:06:36 +0530

    updated

---
+++ paymentapi/Bpmns/BpmDeployService.cs
@@ -0,0 +1,22 @@
+﻿namespace paymentapi.Bpmns
+{
+    public class BpmDeployService : IHostedService
+    {
+        private readonly BpmnService bpmnService;
+        public BpmDeployService(BpmnService bpmnService)
+        {
+            this.bpmnService = bpmnService;
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await bpmnService.DeployProcessDefinition();
+
+            await bpmnService.CleanupProcessInstances();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
